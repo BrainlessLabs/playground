2b67bcde90bbbdd8204af7d27c762331e23cf83b
BrainlessLabs <BrainlessLabs@users.noreply.github.com>
2018-07-26 06:36:13 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -v ./src/Generate.cpp ./src/unqite.c ./src/sqlite/sqlite3.c -o bun
+gcc -I/usr/local/include -I/home/shakti/Downloads/bun-master/include/ -I/home/shakti/Downloads/bun-master/include/third_party -I/home/shakti/Downloads/bun-master/include/third_party/sqlite/ -I/home/shakti/Downloads/bun-master/include/third_party/unqlite/ -I/home/shakti/Downloads/bun-master/include/third_party/msgpack/ ./src/Generate.cpp ./src/unqlite.c ./src/sqlite/sqlite3.c -o bun
